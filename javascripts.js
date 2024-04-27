49e95eeeef0bb5d0f65920c28111a90b926e7c6a
mahdi hoseinian <162808972+madkid2005@users.noreply.github.com>
2024-04-27 14:03:29 +0330

    just dashbord.html

--- javascripts.js
+++ javascripts.js
@@ -19,38 +19,3 @@ function kia() {
 
 }
 select2.addEventListener("mouseover", kia)
-
-const click1 = document.getElementById("click1");
-const showclick1 = document.getElementById("showclick1");
-
-function showclicks1() {
-    showclick1.classList.remove("hiden");
-    showclick2.classList.add("hiden");
-    showclick3.classList.add("hiden");
-}
-click1.addEventListener('click', showclicks1)
-
-
-const click2 = document.getElementById("click2");
-const showclick2 = document.getElementById("showclick2");
-
-function showclicks2() {
-    showclick2.classList.remove("hiden");
-    showclick3.classList.add("hiden");
-    showclick1.classList.add("hiden");
-
-
-}
-click2.addEventListener('click', showclicks2)
-
-
-
-const click3 = document.getElementById("click3");
-const showclick3 = document.getElementById("showclick3");
-
-function showclicks3() {
-    showclick3.classList.remove("hiden");
-    showclick1.classList.add("hiden");
-    showclick2.classList.add("hiden");
-}
-click3.addEventListener('click', showclicks3)
